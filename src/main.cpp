e5d323a5c0afcebf88038620ae71358e4c301974
Alexander <Sasha1_1997@mail.ru>
2017-11-16 02:25:29 +0300

    ch

--- src/main.cpp
+++ src/main.cpp
@@ -37,14 +37,14 @@ int main()
 {
 	testMatrix();
 	Matrix A(2,2), f(2,1);
-	double coef = 10;
+	double coef = 1.0;
 	A[0][0] = 0.2;
 	A[0][1] = 0.1;
 	A[1][0] = 0.1;
 	A[1][1] = 0.2;
 	f[0][0] = 0.1;
 	f[1][0] = -0.1;
-	AltTriang sys(A*coef, f*coef, 14);
+	AltTriang sys(A*coef, f*coef, 13);
 	cout<<sys.solve();
 	return 0;
 }
