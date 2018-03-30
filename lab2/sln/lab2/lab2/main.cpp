53a68b0821de67b388bf4f3e7e5694faf0319154
Панов Александр Алексеевич <panov.a@ccam.cmc.unn.net>
2018-03-30 16:06:12 +0300

    change menu

--- lab2/sln/lab2/lab2/main.cpp
+++ lab2/sln/lab2/lab2/main.cpp
@@ -3,54 +3,71 @@
 #include "calculate_integral.h"
 using namespace std;
 
-void printFunctions(const vector<MathFuncton*> &functions)
+struct CalculateInfo
 {
-    for (int i = 0; i < functions.size(); i++)
+	vector<MathFuncton*> functions;
+	vector<CalculateIntegral*> calculateSchemes;
+
+	int function;
+	int calculateScheme;
+	double a, b;
+	double dx;
+	int N;
+
+	double calculate()
+	{
+		return calculateSchemes[calculateScheme]->calculate(functions[function], a, b, N);
+	}
+};
+
+void printFunctions(const CalculateInfo &calculateInfo)
+{
+    for (int i = 0; i < calculateInfo.functions.size(); i++)
     {
-        cout << i << ' ' << functions[i]->getName()<<'\n';
+        cout << i << ' ' << calculateInfo.functions[i]->getName()<<'\n';
     }
 }
-void printCalculateSchemes(const vector<CalculateIntegral*> &calculateSchemes)
+void printCalculateSchemes(const CalculateInfo &calculateInfo)
 {
-    for (int i = 0; i < calculateSchemes.size(); i++)
+    for (int i = 0; i < calculateInfo.calculateSchemes.size(); i++)
     {
-        cout << i << ' ' << calculateSchemes[i]->getName() << '\n';
+        cout << i << ' ' << calculateInfo.calculateSchemes[i]->getName() << '\n';
     }
 }
-void readFunction(int &f, const vector<MathFuncton*> &functions)
+void readFunction(CalculateInfo &calculateInfo)
 {
     cout << "Choice the function:\n";
-    printFunctions(functions);
-    cin >> f;
+    printFunctions(calculateInfo);
+    cin >> calculateInfo.function;
 }
-void readScheme(int &scheme, const vector<CalculateIntegral*> &calculateSchemes)
+void readScheme(CalculateInfo &calculateInfo)
 {
     cout << "Choice the calculate scheme:\n";
-    printCalculateSchemes(calculateSchemes);
-    cin >> scheme;
+    printCalculateSchemes(calculateInfo);
+    cin >> calculateInfo.calculateScheme;
 }
-void readLimits(double &l, double &r)
+void readLimits(CalculateInfo &calculateInfo)
 {
     cout << "print left and right borders:\n";
-    cin >> l >> r;
+    cin >> calculateInfo.a >> calculateInfo.b;
 }
-void readSize(int &N, double &dx)
+void readSize(CalculateInfo &calculateInfo)
 {
     cout << "print numbers of points\n";
-    cin >> N;
+    cin >> calculateInfo.N;
 }
 
-bool isReady(int function, int calculateScheme, double a, double b, double dx, int N)
+bool isReady(CalculateInfo &calculateInfo)
 {
     return true;
 }
 
-void init(vector<MathFuncton*> &functions, vector<CalculateIntegral*> &calculateSchemes)
+void init(CalculateInfo &calculateInfo)
 {
     Sinus *msin = new Sinus();
     Cosinus* mcos = new Cosinus();
 	LinearFuncton* mlf = new LinearFuncton();
-	functions = { (MathFuncton*)msin , (MathFuncton*)mcos, (MathFuncton*)mlf };
+	calculateInfo.functions = { (MathFuncton*)msin , (MathFuncton*)mcos, (MathFuncton*)mlf };
 
     RightRectangleMethod *m1 = new RightRectangleMethod();
     LeftRectangleMethod *m2 = new LeftRectangleMethod();
@@ -58,28 +75,22 @@ void init(vector<MathFuncton*> &functions, vector<CalculateIntegral*> &calculate
     Monte_KarloMethod *m4 = new Monte_KarloMethod();
 	Gauss_Method *m5 = new Gauss_Method();
 
-	calculateSchemes = { (CalculateIntegral*)m1, (CalculateIntegral*)m2, 
+	calculateInfo.calculateSchemes = { (CalculateIntegral*)m1, (CalculateIntegral*)m2,
 		(CalculateIntegral*)m3, (CalculateIntegral*)m4, (CalculateIntegral*)m5 };
 }
 
 int main()
 {
     Menu menu;
-    vector<MathFuncton*> functions;
-    vector<CalculateIntegral*> calculateSchemes;
+	CalculateInfo calculateInfo;
 
-    int function;
-    int calculateScheme;
-    double a, b;
-    double dx;
-    int N;
-
-    init(functions, calculateSchemes);
+    init(calculateInfo);
 
     menu.push_back("calculate integral"); //Лучше чтобы меню было на русском, а то Эгаму будет не понятно!!!
 	menu.push_back("set integral");
 	menu.push_back("set function");
 	menu.push_back("set limit of integration");
+	menu.push_back("set scheme");
 	menu.push_back("set num point");
     while (1)
     {
@@ -91,33 +102,34 @@ int main()
         }
         else if (menu.choice == "1")
         {
-			if (isReady(function, calculateScheme, a, b, dx, N))
+			if (isReady(calculateInfo))
 			{
-				cout << calculateSchemes[calculateScheme]->calculate(functions[function], a, b, N) << '\n';
+				menu.clear();
+				cout << calculateInfo.calculate() << '\n';
 			}
         }
         else if (menu.choice == "2")
         {
-			readFunction(function, functions);
-			readLimits(a, b);
-			readScheme(calculateScheme, calculateSchemes);
-			readSize(N, dx);           
+			readFunction(calculateInfo);
+			readLimits(calculateInfo);
+			readScheme(calculateInfo);
+			readSize(calculateInfo);
         }
 		else if (menu.choice == "3")
 		{
-			readFunction(function, functions);
+			readFunction(calculateInfo);
 		}
 		else if (menu.choice == "4")
 		{
-			readLimits(a, b);
+			readLimits(calculateInfo);
 		}
 		else if (menu.choice == "5")
 		{
-			readScheme(calculateScheme, calculateSchemes);
+			readScheme(calculateInfo);
 		}
 		else if (menu.choice == "6")
 		{
-			readSize(N, dx);
+			readSize(calculateInfo);
 		}
         else
         {
